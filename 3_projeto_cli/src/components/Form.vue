271501e4215f567b5d0c3daf748272aa0e547213
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-03-28 21:13:19 -0300

    Hierarquia de componentes.

---
+++ 3_projeto_cli/src/components/Form.vue
@@ -0,0 +1,25 @@
+<template>
+    <form action="">
+        <h2>Meu Formulário:</h2>
+        <div>
+            <InputText />
+        </div>
+        <div>
+            <InputText />
+        </div>
+        <div>
+            <Submit />
+        </div>
+    </form>
+</template>
+<script>
+import InputText from './form/InputText.vue'
+import Submit from './form/Submit.vue'
+export default {
+    name: 'Form',
+    components: {
+        InputText,
+        Submit
+    }
+}
+</script>
--- 3_projeto_cli/src/components/Pessoa.vue
+++ 3_projeto_cli/src/components/Pessoa.vue
@@ -1,15 +1,22 @@
 <template>
     <div>
         <h2>Esta é a descrição da pessoa: {{ nome }}</h2>
-        
+        <Info />
+        <Form />
     </div>
 </template>
 <script>
+import Info from './Info'
+import Form from './Form'
 export default {
     nome: 'Pessoa',
+    components: {
+        Info,
+        Form
+    },
     data() {
         return {
-            nome: 'Péricles'
+            nome: 'Calmo, Dedicado, Resiliente.'
         }
     }
 }
