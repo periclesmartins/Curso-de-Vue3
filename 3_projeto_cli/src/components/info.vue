fa1db2ac08556d65b1924db662a2edd94d0123ef
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-03-28 21:34:23 -0300

    Estudando sobre v-if e v-show

--- 3_projeto_cli/src/components/info.vue
+++ 3_projeto_cli/src/components/info.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades</p>
     <p>Últilizo as seguintes tecnologias:</p>
     <ul>
         <li>Html</li>
         <li>Css</li>
         <li>Javascript</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 <script>
 export default {
-  name: 'Info'
+  name: 'Info',
+  data() {
+    return {
+      esta_trabalhando: true,
+      mostrar_email: false,
+      email: 'pericles@gmail.com'
+    }
+  }
 }
 </script>
